418bfd6ded9426869036a2334c20cc1b3edfb1e0
Vitalii Tymkiv <vitaliy.tymkiv@gmail.com>
2022-09-22 15:19:29 +0300

    Before mentors checking

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,24 @@
 // Add imports above this line
+
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 import { galleryItems } from './gallery-items';
+
+import createGalleryItem from './partials/function-create-gallery-item';
+
+import clickOnGalleryLink from './partials/function-click-on-gallery-link';
+
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+const galleryMarkup = createGalleryItem(galleryItems);
+galleryRef.insertAdjacentHTML('beforeend', galleryMarkup);
+
+new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+});
+
+galleryRef.addEventListener('click', clickOnGalleryLink);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
+import throttle from 'lodash.throttle';
 
+const formRef = document.querySelector('.feedback-form');
+
+const STORAGE_KEY = 'feedback-form-state';
+
+import { save, load, remove } from './partials/locale-storage-methods';
+
+startPage();
+
+formRef.addEventListener('input', throttle(formInputter, 500));
+formRef.addEventListener('submit', formSubmiter);
+
+function formInputter(event) {
+  const { name, value } = event.target;
+
+  let savedData = load(STORAGE_KEY);
+  savedData = savedData ? savedData : {};
+
+  savedData[name] = value;
+  save(STORAGE_KEY, savedData);
+}
+
+function startPage() {
+  const savedData = load(STORAGE_KEY);
+
+  if (!savedData) {
+    return;
+  }
+  Object.entries(savedData).forEach(([key, value]) => {
+    formRef.elements[key].value = value;
+  });
+}
+
+function formSubmiter(event) {
+  event.preventDefault();
+  const {
+    elements: { email, message },
+  } = event.currentTarget;
+  console.log({ email: email.value, message: message.value });
+  event.currentTarget.reset();
+  remove(STORAGE_KEY);
+}
